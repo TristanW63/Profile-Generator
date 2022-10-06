f1bcd8a67261762c664f2b32d3c8872f133dcf03
Tristan <tristan_wilson_63@yahoo.com>
2022-10-06 18:18:10 -0500

    adding classes

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateHtml = require('./src/generateHTML');
+const path = require("path");
 
-const questions = [
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require('./src/generateHTML');
+
+const qs = [
         {
             type: 'input',
             name: 'name',
-            message: 'What is the Team Managers name?'
+            message: 'What is the Team Members name?'
         },
         {
             type: 'input',
-            name: 'Id',
+            name: 'id',
             message: 'What is your employee id?'
         },
         {
@@ -20,7 +28,7 @@ const questions = [
         },
         {
             type: 'list',
-            name: 'Role',
+            name: 'role',
             message: 'Pick one',
             choices: ['Engineer', 'Intern', 'Manager'],
         },
@@ -41,13 +49,13 @@ const questions = [
         },
         {
             type: 'input',
-            name: 'Schoolname',
+            name: 'school',
             message: 'What is your Schools name?',
             when: () => ans1.role === 'Intern',
         },
         {
             type: 'input',
-            name: 'Engineergit',
+            name: 'github',
             message: 'What is your Gitbuh username?',
             when: () => ans1.role === 'Engineer',
         },
@@ -57,5 +65,45 @@ const questions = [
             name: "addMember",
           },
         ])
+
+        .then((ans2) => {
+            if(ans1.role === 'Manager') {
+                const manager = new Manager(ans1.name, ans1.id, ans1.email, ans1.role, ans1.officeNumber);
+                team.push(manager);
+            }
+
+            if(ans1.role === 'Engineer') {
+                const engineer = new Engineer(ans1.name, ans1.id, ans1.email, ans1.role, ans1.github);
+                team.push(engineer);
+            }
+
+            if(ans1.role === 'Intern') {
+                const intern = new Intern(ans1.name, ans1.id, ans1.email, ans1.role, ans1.school);
+                team.push(intern);
+            }
+
+            if(ans2.addMember) {
+                generateTeam();
+            } else {
+                team.forEach((team) => {
+                    console.log(team);
+                });
+                fs.writeFile(outputPath, render(team), (err) => {
+                    if (err) {
+                      throw err;
+                    }
+                    console.log("Success, team HTML is created!");
+                  });
+                }
+              });
+          })
+          .catch((err) => {
+            if (err) {
+              throw err;
+            }
+          });
+      };
+      
+      generateTeam();
 
         
