6aeae7b86cc6f0c6d91af309159d0459dcac7328
Tristan <tristan_wilson_63@yahoo.com>
2022-10-10 16:18:02 -0500

    wont display html

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
 const path = require("path");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require('./src/generateHTML');
+const render = require("./lib/generateHTML");
 
 const qs = [
         {
@@ -23,7 +23,7 @@ const qs = [
         },
         {
             type: 'input',
-            name: 'Email',
+            name: 'email',
             message: 'What is your employee email?'
         },
         {
